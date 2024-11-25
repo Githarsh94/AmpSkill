429ab0ef9597e91198f8a98a8d429774ed4e56fd
Githarsh94 <9417harshit@gmail.com>
2024-11-25 13:41:25 +0530

    turned on logger middleware

--- Server/src/middlewares/logger.middleware.ts
+++ Server/src/middlewares/logger.middleware.ts
@@ -2,12 +2,13 @@ import { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 
 const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const log = `[${new Date().toISOString()}] ${req.method} ${req.url}\n`;
-    fs.appendFile('./src/logs/logs.txt', log, (err) => {
-        if (err) {
-            console.error('Error writing to log file:', err);
-        }
-    });
+    const log = `[${new Date().toISOString()}] ${req.method} ${req.url} - ${req.ip}\n`;
+    // fs.appendFile('./src/logs/logs.txt', log, (err) => {
+    //     if (err) {
+    //         console.error('Error writing to log file:', err);
+    //     }
+    // });
+    console.log(log);
     next();
 };
 
